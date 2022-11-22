a1e8876c7d6266319472d74613f06450fcdcf04c
Adam Sargent <adamcsargent@gmail.com>
2022-11-22 16:02:05 -0500

    Remove unused App.css; Remove unused routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Deals, Form, Invoice, PaymentSchedule } from "./screens/index";
 import { Welcome } from "./screens/Welcome/Welcome";
 
 function App() {
@@ -9,10 +7,6 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Welcome />} />
-                <Route path="/deals" element={<Deals />} />
-                <Route path="/form" element={<Form />} />
-                <Route path="/schedule" element={<PaymentSchedule />} />
-                <Route path="/invoice" element={<Invoice />} />
             </Routes>
         </BrowserRouter>
     );
